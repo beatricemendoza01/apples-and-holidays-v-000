952041d5cd16d50717a44588bff55163d8aaeac9
Beatrice Mendoza <mendoza@nyu.edu>
2018-09-24 23:50:11 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,7 +54,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  
+  holiday_hash.each do |season, holidays|
+    puts "#{season.to_s}: #{holidays}"
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
