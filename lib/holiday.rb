6f4554d477e86204921498a4bb9d8b5b8d529583
Beatrice Mendoza <mendoza@nyu.edu>
2018-09-25 00:00:06 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.to_s.capitalize}:"
     holidays.each do |holiday, supplies|
       holiday = holiday.to_s.split("_").each { |word| word.capitalize! }
-      puts "  #{holiday.join}: #{supplies.join(", ")}"
+      puts "  #{holiday.join(" ")}: #{supplies.join(", ")}"
     end
   end
 end
